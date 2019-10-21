3eb1af58e94b8c70f703e0e1e7106717a8f39629
Rajat2809 <36286079+Rajat2809@users.noreply.github.com>
2019-10-20 22:38:02 -0700

    Done changes for user creation and Login before add to cart

--- user/index.js
+++ user/index.js
@@ -1,5 +1,45 @@
 const _ = require("lodash");
+const db = require("../db");
+const reqst = require("request");
 
+
+module.exports.getUser = function(req,res){
+     let {email, pass} = {...req.query};
+     console.log(email);
+     db.User.findOne({where:{username:email}}).then(user=>{
+         if(user){
+         var u = JSON.parse(JSON.stringify(user));
+         if(u.password == pass)
+         res.send([true,u]);
+         else
+         res.send([false,null]);
+        }
+        else
+        res.send([false,null]);
+     });
+}
+module.exports.registerUser = function(req,res){
+    let{email, pass, confirmPass} ={...req.query};
+  if(pass && confirmPass && pass===confirmPass){
+      let query = {
+    username: email,
+    password: pass,
+    createdAt: Date.now(),
+    updatedAt: Date.now()};
+    console.log("Password Match");
+    console.log(email);
+    console.log(pass);
+    db.User.create(query).done((err,res)=>{
+        console.log(res);
+        console.log(err);
+    if(err){
+        console.log(err.stack);
+    }else{
+        console.log(res);
+    }
+    });
+  }
+}
 module.exports.postRegister = function(req, res) {
     let {ownerName, userName, password} = {...req.body};
     console.log("ownerName" + ownerName);
